e3980685bd6c712e241342107c89c07b3a34b049
sdtsui <danielt213@gmail.com>
2014-12-22 17:51:20 -0800

    Recursive tree creation

--- script/data.js
+++ script/data.js
@@ -1,27 +1,44 @@
 //Data Manipulation
-var Tree = function(x,y){
+var Tree = function(x, y, width, color){
 	var tree = {};
-
-	tree.root = {x: x, y:y}
-	//level of tree, also represent x
-	tree.level = 0;
-
-	//level of tree, also represent height
-	tree.height = 20;
-
+	tree.root = {'x': x, 'y': y};
+	tree.height = findRandom(200,100);
+	tree.width = width;
+	tree.color = color; 
+	tree.angle = findRandom(55,-55); // Denominated in degrees.
+	tree.isLeaf = true;
 	tree.children = [];
-	tree.drawLeave = function(){
-		//should return true if no children
-	}
-	tree.insert = function(x,y){
-		//should insert new tree as children
+
+	tree.insert = function(){
+		//Create new root (x,y), 
+		var newRoot = nextRoot.call(this);
+		var newTree = Tree(newRoot.x, newRoot.y, this.width/2, this.color);
+		this.children.push(newTree);
+		return newTree;
 
 	};
 	tree.visualize = function(){
 		//should return an array of all children ready for d3
 	};
-}
 
-var tree = Tree(250,750);
-tree.visualize();
+	var nextRoot = function(){
+		//takes in the current Tree's x, y, and outputs an object with the next level's root
+		var radian = toRadian(this.angle);
+		newX = this.root.x + this.height * (Math.sin(radian));
+		newY = this.root.y - this.height * (Math.cos(Math.abs(radian)));
+		return {'x': newX, 'y': newY};
+	};
+	var toDegree = function(radian){
+		return radian*(180/Math.PI);
+	};
+	var toRadian = function(degree){
+		return degree*(Math.PI/180);
+	}
+
+	return tree;
+};
+
+var findRandom = function(max,min){
+	return Math.floor(Math.random()*(max-min))+ min;
+}
 
--- script/init.js
+++ script/init.js
@@ -1,26 +1,43 @@
 var svgWidth = 500;
 var svgHeight = 750;
+var allTrees = [];
+
+var rootTree = Tree(svgWidth/2, svgHeight, 25, 'black');
+rootTree.angle = 0;       
+allTrees.push(rootTree);
+
+for (var i = 0; i < 5; i++){
+	allTrees.push(rootTree.insert());
+	for(var j = 0; j < 5; j++){
+		allTrees.push(rootTree.children[i].insert())
+	}
+}
+
 
 var svg = d3.select('body').append('svg')
 						.attr('class','tree')
 						.attr('width',svgWidth)
 						.attr('height',svgHeight)
 
-svg.selectAll('rect').data([{width:50,height:200,level:1}])
+svg.selectAll('rect').data(allTrees)
 	.enter().append('rect')
-		.attr('x',function(d){return svgWidth/2-d.width/2})
-		.attr('y',function(d){return svgHeight-d.height*d.level})
+		.attr('x',function(d){return d.root.x - d.width/2})
+		.attr('y',function(d){return d.root.y-d.height})
 		.attr('height',function(d){return d.height})
 		.attr('width',function(d){return d.width})
-		.attr('fill','black')
-		//.attr('transform','rotate(-45 250 750)')
+		.attr('fill', function(d){return d.color})
+		.attr('transform',function(d){return 'rotate(' + d.angle + ' ' + d.root.x + ' ' + d.root.y + ')'})
 
+
+console.dir(allTrees);
+/*
 d3.select('svg').append('circle')
 	.attr('cx',250)
 	.attr('cy',750)
 	.attr('r','10')
 	.attr('fill','red')
-
+*/
+/*
 svg.selectAll('rect').data([{},{width:50,height:200,level:2,ox:250,oy:550}])
 	.enter().append('rect')
 		.attr('x',function(d){return svgWidth/2-d.width/2})
@@ -38,3 +55,4 @@ svg.selectAll('rect').data([{},{},{width:50,height:200,level:3}])
 		.attr('width',function(d){return d.width})
 		.attr('fill','green')
 		//.attr('transform','rotate(-25  550)')
+*/
