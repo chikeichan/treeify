a2184b75ee97da0edfda43c2a29889bc616bf500
sdtsui <danielt213@gmail.com>
2014-12-24 17:45:24 -0800

    V0.01: Refactor and documentation.

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 var http = require('http');
 var express = require('express');
 var path = require('path');
-var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://test:test@ds053130.mongolab.com:53130/angular-book')
-var Schema = mongoose.Schema;
 var bodyParser = require('body-parser');
 
 var app = express();
@@ -14,14 +11,7 @@ app.use(bodyParser.urlencoded({
 
 var dom;
 var extractedDom;
-
 var request = require("request");
-
-//
-app.get('/api/url', function(req,res){
-	res.send(extractedDom);
-	
-});
 
 app.post('/api/url',function(req,res){
 	if(!!req.body.query){
@@ -29,23 +19,15 @@ app.post('/api/url',function(req,res){
 		  uri: req.body.query,
 		}, function(error, response, body) {
 		  extractedDom = response.body;
+		  res.send(extractedDom);
 		});
 	}
 })
-
-
-
 //Express: Set Port 
 app.set('port', process.env.PORT || 8080);
 
-//Express: use view engine jade as template. Look into /views subfolder
-app.set('../', path.join(__dirname,'../'));
-app.set('view engine', 'jade');
-
-
 //Express: serve base route / to localhost8080
 app.use(express.static(path.join(__dirname,'/')));
-
 app.use(function(req,res){
 	res.render('404',{url: req.url});
 })
